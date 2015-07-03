69dc5c327dd63726dac6a35a057c4896044e7e63
Taurvi <srimbakusumo@gmail.com>
2015-07-02 17:10:39 -0700

    Finished login functions

--- tasks/js/signin.js
+++ tasks/js/signin.js
@@ -7,3 +7,22 @@
 */
 
 //use jQuery to register a function that is called when the document is ready for manipulation
+$(function() {
+    $('.form-signin').submit(function(evt) {
+
+        evt.preventDefault();
+
+        // Variables
+        var email = $('#inputEmail');
+        var password = $('#inputPassword');
+
+        // Login function, returns a promise
+        Parse.User.logIn(email, password).then(function() {
+            // Redirect on success
+            window.location = 'index.html';
+        }, function(err){
+            // Display error on fail
+            showError(err);
+        });
+    })
+})
